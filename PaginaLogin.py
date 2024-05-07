b7c2f3042bfcc895602633e6ba022aee1ac4cfc3
EmillyMarrocos <emillymarrocos5@gmail.com>
2024-05-07 17:36:07 -0300

    Terceiro Commit da PagLogin_Kivy

--- PaginaLogin.py
+++ PaginaLogin.py
@@ -3,47 +3,41 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.core.window import Window
-
-Window.clearcolor = (0.5, 0, 0.5, 1)
+from kivy.uix.image import Image
 
 class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        #Layout
+        super(LoginScreen, self).__init__(**kwargs)
+        
+        # Define o layout
         self.orientation = 'vertical'
+        self.spacing = 10
+        self.padding = [50, 20, 50, 20]
+        
+        # Adiciona o ícone de perfil
+        self.add_widget(Image(source='Users/aluno.sesipaulista/Documents/PagLogin_Kivy/img_icone/user_icon.png"', size_hint_y=None, height=150))
 
-        #Labels
-        self.add_widget(Label(text='Login', size_hint=(1, 0.5)))
+        # Adiciona o Label de Login
+        self.add_widget(Label(text='Login', size_hint=(1, 0.2)))
 
-        #Inputs
-        self.email_input = TextInput(hint_text='Email', size_hint=(1, 0.5))
+        # Adiciona o input de email
+        self.email_input = TextInput(hint_text='Email', size_hint=(1, 0.1))
         self.add_widget(self.email_input)
-
-        self.password_input = TextInput(hint_text='Senha', password= True, size_hint=(1, 0.5))
+        
+        # Adiciona o input de senha
+        self.password_input = TextInput(hint_text='Senha', password=True, size_hint=(1, 0.1))
         self.add_widget(self.password_input)
-
-        #Buttons
-        self.login_button = Button(text='Entrar', on_press= self.login, size_hint=(1, 0.5))
+        
+        # Adiciona os botões de entrar e cadastrar
+        self.login_button = Button(text='Entrar', size_hint=(1, 0.1), background_color=(0.5, 0, 1, 1))
         self.add_widget(self.login_button)
-
-        self.register_button = Button(text='Cadastrar', on_press=self.register, size_hint=(1, 0.5))
+        
+        self.register_button = Button(text='Cadastrar', size_hint=(1, 0.1), background_color=(0.5, 0, 1, 1))
         self.add_widget(self.register_button)
 
-    def login(self, instance):
-        #Implementar a lógica de Login
-        email = self.email_input.text
-        senha = self.password_input.text
-        print('Email: ', email, 'Senha: ', senha)
-
-    def register(self, instance):
-        #Implementar a lógica de Cadastro
-        print('Usuário cadastrado com sucesso!')
-
-class LoginApp(App): 
+class LoginApp(App):
     def build(self):
         return LoginScreen()
-    
+
 if __name__ == '__main__':
     LoginApp().run()
