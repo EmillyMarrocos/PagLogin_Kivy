4c6389da38ed534cc7fceeff73e26f3aef0df7e7
EmillyMarrocos <emillymarrocos5@gmail.com>
2024-05-07 15:22:28 -0300

    Segundo commit da PagLogin_Kivy

--- PaginaLogin.py
+++ PaginaLogin.py
@@ -3,6 +3,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.core.window import Window
+
+Window.clearcolor = (0.5, 0, 0.5, 1)
 
 class LoginScreen(BoxLayout):
     def __init__(self, **kwargs):
@@ -36,7 +39,7 @@ class LoginScreen(BoxLayout):
 
     def register(self, instance):
         #Implementar a lógica de Cadastro
-        print('Ainda não tem uma conta? Cadastre-se')
+        print('Usuário cadastrado com sucesso!')
 
 class LoginApp(App): 
     def build(self):
